a6f64d50e8edd77ade73aebef99f49fa6d943299
Nicolas Orellana <norellan@gmail.com>
2022-08-12 12:38:05 -0400

    gh-pages add

---
+++ React-Galery-main 2/build/precache-manifest.f528a5d893a2fac35da594e145424db6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a732e273296e15aaed80",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/css/main.adc03361.chunk.css"
+  },
+  {
+    "revision": "a732e273296e15aaed80",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/main.a732e273.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "b4e9b9f34a45bdaa03e9",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/css/2.9a2a451b.chunk.css"
+  },
+  {
+    "revision": "b4e9b9f34a45bdaa03e9",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/2.b4e9b9f3.chunk.js"
+  },
+  {
+    "revision": "e7f841cc3168cec3b6cb",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/runtime~main.e7f841cc.js"
+  },
+  {
+    "revision": "b4ecbc4872b2c7725444c90bf8979e1e",
+    "url": "/Desafio-React-1-Galeria-Imagenes/index.html"
+  }
+];
--- React-Galery-main 2/src/components/layout/header.jsx
+++ React-Galery-main 2/src/components/layout/header.jsx
@@ -1,18 +1,18 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Stack from 'react-bootstrap/Stack';
-import React, { Component }  from 'react';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Stack from "react-bootstrap/Stack";
+import React, { Component } from "react";
 
 const NavbarComponent = (props) => {
   return (
-        <Navbar expand="lg" variant="dark" bg="dark">
-          <Container>
-            <Stack gap={2} className="col-md-5 mx-auto">
-                <Navbar.Brand className="text-center">{props.title}</Navbar.Brand>
-            </Stack>
-          </Container>
-        </Navbar>
-      );
-}
+    <Navbar expand="lg" variant="dark" bg="dark">
+      <Container>
+        <Stack gap={2} className="col-md-5 mx-auto">
+          <Navbar.Brand className="text-center">{props.title}</Navbar.Brand>
+        </Stack>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavbarComponent;
