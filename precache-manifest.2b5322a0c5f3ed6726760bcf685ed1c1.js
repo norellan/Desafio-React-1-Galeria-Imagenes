da9752ffd4de858de2634487cfc2cadb0dff30d6
Nicolas Orellana <norellan@gmail.com>
2022-08-12 12:41:38 -0400

    Updates

---
+++ precache-manifest.2b5322a0c5f3ed6726760bcf685ed1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcdc14e7a1623729bea9",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/css/main.bb9c656b.chunk.css"
+  },
+  {
+    "revision": "fcdc14e7a1623729bea9",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/main.fcdc14e7.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "b4e9b9f34a45bdaa03e9",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/css/2.9a2a451b.chunk.css"
+  },
+  {
+    "revision": "b4e9b9f34a45bdaa03e9",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/2.b4e9b9f3.chunk.js"
+  },
+  {
+    "revision": "e7f841cc3168cec3b6cb",
+    "url": "/Desafio-React-1-Galeria-Imagenes/static/js/runtime~main.e7f841cc.js"
+  },
+  {
+    "revision": "ecb2a626a87d84f68367705707cce347",
+    "url": "/Desafio-React-1-Galeria-Imagenes/index.html"
+  }
+];
